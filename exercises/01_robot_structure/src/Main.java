86588e76e8d09468afbb4805de2c5d19a61ea445
Vishnu Bharath <mr.vishnubharath@gmail.com>
2025-10-30 14:09:12 -0400

    refactor + add exercise

---
+++ exercises/01_robot_structure/src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Entry point for the Robot Simulation.
+ * This simulates what the 'Robot' class would do in FRC.
+ * need to create the other Robot class
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Starting Robot Simulation ===");
+        //Robot robot = new Robot();
+        //robot.start();
+    }
+}
